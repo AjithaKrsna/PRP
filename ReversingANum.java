cb660cab2eaf1f8c1c76e604c261e1221e4cffca
AjithaKrsna <42969470+AjithaKrsna@users.noreply.github.com>
2019-06-21 12:21:47 +0530

    Create ReversingANum.java

---
+++ ReversingANum.java
@@ -0,0 +1,16 @@
+package FlowCtrls;
+import java.util.*;
+public class ReversingANum {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int rev=0;
+		while(n!=0){
+			int m=n%10;
+			rev=rev * 10+m;
+			n=n/10;
+		}
+		System.out.println(rev);
+	}
+
+}
